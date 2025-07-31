966a9bde1d4efb8c8dcaa8a2cdd30880d2301825
Lex O'brien <lex.e.obrien@gmail.com>
2025-07-31 22:26:42 +0100

    feat: Add initial molviewspec animation creator

---
+++ mvsj_creator.py
@@ -0,0 +1,17 @@
+import molviewspec as mvs
+
+def create_animation(): 
+    """
+    Create a 2 state animation using the pdb 5vpy
+    State 1: 
+    - title: 'State 1'
+    - description: 'The protein and ligand in canonical representation'
+    - show the protein in cartoon representation.
+    - show the ligand in ball and stick representation
+    State 2: 
+    - title 'State 2'
+    - description: 'The switch II region' 
+    - highlight the residues 22 - 44 in a different colour. 
+    """
+
+
